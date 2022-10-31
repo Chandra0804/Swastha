e247fbcad1f868607cb1225d9cdf60d6286ee230
Bhanu Prakash <97606851+bhanuprakash16@users.noreply.github.com>
2022-10-31 14:51:11 +0530

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,38 +1,24 @@
 //Person Class
-class Person {
+abstract class Person {
 	// Fields
 	protected String Name;
 	protected String DateOfBirth;
-	protected String AadharNumber;
+	protected String ID;
 	
 	protected String Gender;
-	protected String BloodGroup;
 	
-	protected String AddressLine;
-	protected String Landmark;
-	protected String City;
+	protected String Address;
 	protected int Pincode;
 	
-	protected String District;
-	protected String State;
-	
 	// << Constructor >>
-	public Person (String name, String dob, String aadhar, String gender, String bloodGroup,
-			String dno_street, String landmark, String city, String district, String state, int pincode) {
+	public Person (String name, String dob, String gender, String address, int pincode) {
 		this.Name = name;
 		this.DateOfBirth = dob;
-		this.AadharNumber = aadhar;
 		
 		this.Gender = gender;
-		this.BloodGroup = bloodGroup;
 		
-		this.AddressLine = dno_street;
-		this.Landmark = landmark;
-		this.City = city;
+		this.Address = address;
 		this.Pincode = pincode;
-		
-		this.District = district;
-		this.State = state;
 	}
 	
 	// Respective Accessors and Mutators
@@ -51,14 +37,6 @@ class Person {
 		return this.DateOfBirth;
 	}
 	
-	public void setAadhar (String aadhar) {
-		this.AadharNumber = aadhar;
-	}
-	
-	public String getAadhar () {
-		return this.AadharNumber;
-	}
-	
 	public void setGender (String gender) {
 		this.Gender = gender;
 	}
@@ -67,36 +45,12 @@ class Person {
 		return this.Gender;
 	}
 	
-	public void setBloodGroup (String bloodGroup) {
-		this.BloodGroup = bloodGroup;
+	public void setAddressLine(String address) {
+		this.Address = address;
 	}
 	
-	public String getBloodGroup () {
-		return this.BloodGroup;
-	}
-	
-	public void setAddressLine(String addressLine) {
-		this.AddressLine = addressLine;
-	}
-	
-	public String getAddressLine() {
-		return this.AddressLine;
-	}
-	
-	public void setLandmark (String landmark) {
-		this.Landmark = landmark;
-	}
-	
-	public String getLandmark () {
-		return this.Landmark;
-	}
-	
-	public void setCity (String city) {
-		this.City = city;
-	}
-	
-	public String getCity () {
-		return this.City;
+	public String getAddress() {
+		return this.Address;
 	}
 	
 	public void setPincode (int pincode) {
@@ -107,19 +61,5 @@ class Person {
 		return this.Pincode;
 	}
 	
-	public void setDistrict (String district) {
-		this.District = district;
-	}
-	
-	public String getDistrict () {
-		return this.District;
-	}
-	
-	public void setState (String state) {
-		this.State = state;
-	}
-	
-	public String getState() {
-		return this.State;
-	}
+	public abstract int ID();
 }
