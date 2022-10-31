2eb6f5d7671111270733470e0dc1d523066c945e
Chandra0804 <97542837+Chandra0804@users.noreply.github.com>
2022-10-31 15:09:46 +0530

    Create Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,38 @@
+//Doctor Class
+class Doctor extends Person{
+	
+	public Doctor(String name, String dob, String gender, String address, int pincode) {
+		super(name, dob, gender, address, pincode);
+	}
+
+	protected int DoctorID;
+	protected String Specialization;
+	protected String Hospital_ID;
+	
+	public void set_Hospital_ID(String Hospital_ID)
+	{
+		this.Hospital_ID= Hospital_ID;
+	}
+	
+	public String get_Hospital_ID()
+	{
+		return Hospital_ID;
+	}
+	
+	public void set_Specialization(String Specialization)
+	{
+		this.Specialization = Specialization;
+	}
+	
+	public String get_Specailization()
+	{
+		return Specialization;
+	}
+	
+	@Override
+	public int Create_ID()
+	{
+		
+	}
+	
+}
