9d7383ced5f4a78f37b670ded32fa559a91b47ce
Chandra0804 <97542837+Chandra0804@users.noreply.github.com>
2022-11-03 15:09:11 +0530

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,46 +1,71 @@
-package application;
-
-class Doctor extends Person{
-	
-	public Doctor(String name, String dob, String gender, String address, int pincode) {
-		super(name, dob, gender, address, pincode);
-	}
-
-	protected String Specialization;
-	protected String Hospital_ID;
-	
-	public void set_Hospital_ID(String Hospital_ID)
-	{
-		this.Hospital_ID= Hospital_ID;
-	}
-	
-	public String get_Hospital_ID()
-	{
-		return Hospital_ID;
-	}
-	
-	public void set_Specialization(String Specialization)
-	{
-		this.Specialization = Specialization;
-	}
-	
-	public String get_Specailization()
-	{
-		return Specialization;
-	}
-	
-	@Override
-	public int login(String email , String pass)
-	{
-		if(super.Email.equals(email))
-		{
-			if(super.Password.equals(pass))
-			{
-				return 1;
-			}
-			else return 0;
-		}
-		else return 0;
-	}
-	
-}
+package application;                                                                                                               
+                                                                                                                                   
+import java.util.ArrayList;                                                                                                        
+                                                                                                                                   
+class Doctor extends Person{                                                                                                       
+	                                                                                                                               
+	public Doctor(String name, String dob, String gender, String address, int pincode) {                                           
+		super(name, dob, gender, address, pincode);                                                                                
+	}                                                                                                                              
+                                                                                                                                   
+	protected String Specialization;                                                                                               
+	protected String Hospital_ID;                                                                                                  
+	protected double fees;                                                                                                         
+	ArrayList<Appointments> Patient_list = new ArrayList<Appointments>();                                                          
+	protected int Patient_index = 1;                                                                                               
+	protected Patient p;                                                                                                           
+	                                                                                                                               
+	public void setHospital_ID(String Hospital_ID)                                                                                 
+	{                                                                                                                              
+		this.Hospital_ID= Hospital_ID;                                                                                             
+	}                                                                                                                              
+	                                                                                                                               
+	public String getHospital_ID()                                                                                                 
+	{                                                                                                                              
+		return Hospital_ID;                                                                                                        
+	}                                                                                                                              
+	                                                                                                                               
+	public void setSpecialization(String Specialization)                                                                           
+	{                                                                                                                              
+		this.Specialization = Specialization;                                                                                      
+	}                                                                                                                              
+	                                                                                                                               
+	public String getSpecailization()                                                                                              
+	{                                                                                                                              
+		return Specialization;                                                                                                     
+	}                                                                                                                              
+	                                                                                                                               
+	public void setfees(double fees){                                                                                              
+		this.fees = fees;                                                                                                          
+	}                                                                                                                              
+	                                                                                                                               
+	public double getfees(){                                                                                                       
+		return fees;                                                                                                               
+	}                                                                                                                              
+	                                                                                                                               
+	public int add_Patient(Patient p , String time , String date)                                                                  
+	{                                                                                                                              
+		Patient_list.add(new Appointments(p,time,date));                                                                           
+		return Patient_index++;                                                                                                    
+	}                                                                                                                              
+	                                                                                                                               
+	public void remove_Patient(int index)                                                                                          
+	{                                                                                                                              
+		Patient_list.remove(index);                                                                                                
+	}                                                                                                                              
+	                                                                                                                               
+	@Override                                                                                                                      
+	public int login(String email , String pass)                                                                                   
+	{                                                                                                                              
+		if(super.Email.equals(email))                                                                                              
+		{                                                                                                                          
+			if(super.Password.equals(pass))                                                                                        
+			{                                                                                                                      
+				return 1;                                                                                                          
+			}                                                                                                                      
+			else return 0;                                                                                                         
+		}                                                                                                                          
+		else return 0;                                                                                                             
+	}                                                                                                                              
+	                                                                                                                               
+}                                                                                                                                  
