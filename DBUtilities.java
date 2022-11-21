dedfb97d19d7e93ab229b63fb0d4160cac021a9f
Chandra0804 <97542837+Chandra0804@users.noreply.github.com>
2022-11-21 15:31:31 +0530

    Update DBUtilities.java

--- DBUtilities.java
+++ DBUtilities.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -5,9 +6,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.stage.Stage;
 
 public class DBUtilities {
+
+    public void changeScene(ActionEvent event , String fxmlfile) throws IOException{
+        Parent root = null;
+        FXMLLoader loader = new FXMLLoader(DBUtilities.class.getResource(fxmlfile));
+        root = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    }
 
     public static void signupDoctor(Doctor doc)
     {
@@ -19,7 +33,7 @@ public class DBUtilities {
         try {
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/swastha", "root","MOHAN RAO");
             psCheckUserExists = con.prepareStatement("select * from doctor where email = ?");
-            psCheckUserExists.setString(1, doc.getEmail());
+            psCheckUserExists.setString(1, doc.getemail());
             rs = psCheckUserExists.executeQuery();
 
             if(rs.isBeforeFirst()){
@@ -36,7 +50,7 @@ public class DBUtilities {
                 psInsert.setInt(5, doc.getPincode());
                 psInsert.setString(6, doc.getSpecailization());
                 psInsert.setString(7, doc.getHospital_ID());
-                psInsert.setDouble(8, doc.getFees());
+                psInsert.setDouble(8, doc.getfees());
                 
             }
         } catch (Exception e) {
@@ -89,19 +103,28 @@ public class DBUtilities {
     public static void logindoctor(Doctor doc) {
         Connection con = null;
         PreparedStatement ps = null;
+        PreparedStatement stmt = null;
         ResultSet rs = null;
 
         try {
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/swastha", "root","MOHAN RAO");
-            ps = con.prepareStatement("select password from person where email = ?");
-            
-            rs = ps.executeQuery("select password from person where email = '" + doc.getEmail() + "'");
-            if (rs.next()) {
-                /*
-                 * if ((doc.login(doc.getemail(),rs.getString(1)))) {
-                 * output.setText("Login Success");
-                 * }
-                 */
+            ps = con.prepareStatement("select * from doctor where email = ?");
+            ps.setString(1,doc.getemail());
+            rs = ps.executeQuery();
+            if (rs.isBeforeFirst()) {
+                stmt = con.prepareStatement("select * from doctor where email = ? and password = ?");
+                stmt.setString(1, doc.getemail());
+                rs = stmt.executeQuery();
+                if(rs.isBeforeFirst())
+                {
+
+                }
+
+            }
+            else{
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("No account found with entered email , please recheck your entered email");
+                alert.show();
             }
         } catch (Exception e) {
             System.err.println(e);
