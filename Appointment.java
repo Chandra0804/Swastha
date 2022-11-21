98ba3393ae08bd3db57ac83ee11b5c0df1cb6b0b
Tarun <104731395+Tarun-pvc@users.noreply.github.com>
2022-11-21 19:06:44 +0530

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -1,48 +1,57 @@
-package application;
 
-class Appointments{
-	
- protected String Time;
- protected String Date;
- protected Doctor doc;
- protected Patient p;
- 
- public Appointments(Doctor doc ,String time, String date ){
- 
-    this.Time = time;
-    this.Date = date;
-    this.doc = doc;
-    
- }
- 
- public Appointments(Patient p ,String time, String date ){
-	 
-	    this.Time = time;
-	    this.Date = date;
-	    this.p = p;
- }
+package swastha;
 
- public void setTime(String time){
-    this.Time = time;
- }
+import java.util.UUID;
 
- public String getTime(){
-    return Time;
- }
+public class Appointments {
 
- public void setDate(String date){
-    this.Date = date;
- }
+    protected String id; // uuid
+    protected String Time;
+    protected String Date;
+    protected Doctor doc;
+    protected Patient p;
 
- public String getDate(){
-    return Date;
- }
- 
- public Doctor getDoctor(){
-	 return doc;
- }
- 
- public Patient getPatient(){
-	 return p;
- }
+    public Appointments(Doctor doc, String time, String date) {
+
+        this.Time = time;
+        this.Date = date;
+        this.doc = doc;
+        this.id = UUID.randomUUID().toString();
+
+    }
+
+    public Appointments(Patient p, String time, String date) {
+
+        this.Time = time;
+        this.Date = date;
+        this.p = p;
+    }
+
+    public void setTime(String time) {
+        this.Time = time;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+
+    public void setDate(String date) {
+        this.Date = date;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public Doctor getDoctor() {
+        return doc;
+    }
+
+    public Patient getPatient() {
+        return p;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
